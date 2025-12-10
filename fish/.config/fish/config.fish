98ed6fc285ec0b0df53e55ca64f6ba6972afda94
Cory G Watson <gphat@onemogin.com>
2025-12-10 09:40:54 -0600

    More setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -fx theme_color_scheme dracula
 end
 
 fish_add_path /opt/homebrew/bin
@@ -7,3 +8,5 @@ fish_add_path /opt/homebrew/bin
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx PATH $PATH /opt/nvim/
 set -gx EDITOR nvim
+
+alias vi='nvim'
