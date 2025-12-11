decc03ad3470b7f2061954abc3917f3d48ea2c38
Cory G Watson <gphat@onemogin.com>
2025-12-10 20:49:55 -0600

    Some theme and font fixes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,9 +4,15 @@ if status is-interactive
 end
 
 fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.local/bin
 
 set -gx XDG_CONFIG_HOME ~/.config
-set -gx PATH $PATH /opt/nvim/
 set -gx EDITOR nvim
 
 alias vi='nvim'
+
+# FZF Dracula
+set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
+# Starship
+starship init fish | source
