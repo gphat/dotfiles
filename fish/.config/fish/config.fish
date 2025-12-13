1652940636bba3d9342d8cffb1e5748131832b95
Cory G Watson <gphat@onemogin.com>
2025-12-13 16:59:19 -0600

    Fix non-interactive shells

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -fx theme_color_scheme dracula
+    fastfetch
+    alias vi='nvim'
+
+    # FZF Dracula
+    set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+
+    # Starship
+    starship init fish | source
 end
 
 fish_add_path /opt/homebrew/bin
@@ -8,14 +16,3 @@ fish_add_path $HOME/.local/bin
 
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx EDITOR nvim
-
-alias vi='nvim'
-
-# FZF Dracula
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
-
-# Starship
-starship init fish | source
-
-fastfetch
-
